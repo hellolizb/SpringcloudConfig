ce9bb6c9513670bf237f4a3a7d25444d81eb4a43
hellolizb <43568429+hellolizb@users.noreply.github.com>
2019-04-26 15:16:47 +0800

    Add files via upload

---
+++ 2019-04-26 prp_file_download.sql
@@ -0,0 +1,26 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.5.5-10.0.10-MariaDB-V2.0R131D001-20160907-1111 : Database - channeldb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`channeldb` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `channeldb`;
+
+/*Data for the table `prp_file_download` */
+
+insert  into `prp_file_download`(`id`,`file_name`,`file_url`,`file_type`,`create_date`,`create_code`,`update_date`,`update_code`) values (1,'《客户授权书》.pdf',NULL,'1','2018-11-09 09:12:05','1','2018-11-09 09:12:08','1');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
